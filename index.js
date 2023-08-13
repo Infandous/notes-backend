f579902085418304180abfcbb17a97a6087c8b11
Infandous <imobrady@ymail.com>
2023-08-12 19:14:23 -0500

    implemented mongo functionality to various functions

--- index.js
+++ index.js
@@ -9,31 +9,6 @@ app.use(express.static('build'))
 app.use(express.json())
 const mongoose = require('mongoose')
 
-//do not hardcode password
-/*const password = process.argv[2]
-
-const url = 
-    `mongodb+srv://fullstack:${password}@fullstackcluster.ftx3y6g.mongodb.net/noteApp?retryWrites=true&
-    w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content:String,
-    important:Boolean,
-})
-
-noteSchema.set('toJSON',{
-    transform: (document,returnedObject)=>{
-        returnedObject.id=returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Note = mongoose.model('notes',noteSchema)
-*/
 
 let notes = [  
     {    id: 1,    content: "HTML is easy",    important: true  },  
@@ -54,13 +29,12 @@ app.get('/api/notes',(request,response)=>{
 })
 
 app.get('/api/notes/:id',(request,response)=>{
-    const id = Number(request.params.id)
+    /*const id = Number(request.params.id)
     const note = notes.find(note=>note.id === id)
-    note ? response.json(note) : response.status(404).end()
-    //response.json(note)
-    /*notes.find({id:id}).then(note=>{
+    note ? response.json(note) : response.status(404).end()*/
+    Note.findById(request.params.id).then(note=>{
         response.json(note)
-    })*/
+    })
 })
 
 app.delete('/api/notes/:id', (request,response)=>{
@@ -77,21 +51,19 @@ const generateId = () => {
 }
 app.post('/api/notes',(request,response)=>{
     const body = request.body
-    if (!body.content){
-        return response.status(400).json({
-            error: 'content missing'
-        })
+
+    if (body.content===undefined){
+        return response.status(400).json({error: "content missing"})
     }
-    
-    const note = {
+
+    const note = new Note({
         content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+        important: body.important || false
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote=>{
+        response.json(savedNote)
+    })
 })
 
 const PORT = process.env.PORT || 3001
@@ -99,12 +71,3 @@ app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
 })
 
-
-/*const app = http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-Type':'text/plain'})
-    response.end(JSON.stringify(notes))
-})
-
-const PORT = 3001
-app.listen(PORT)
-console.log(`server running on ${PORT}`)*/
